fc933bcac60455518a4a4ce497e9afb8b6bb6c3f
Victor Marquez <72099625+victorsheepman@users.noreply.github.com>
2024-08-15 09:57:12 -0400

    Initial Commit

---
+++ PruebaTecnicaIDigital/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PruebaTecnicaIDigital
+//
+//  Created by Victor Marquez on 15/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PruebaTecnicaIDigital/PruebaTecnicaIDigitalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaTecnicaIDigitalApp.swift
+//  PruebaTecnicaIDigital
+//
+//  Created by Victor Marquez on 15/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PruebaTecnicaIDigitalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
